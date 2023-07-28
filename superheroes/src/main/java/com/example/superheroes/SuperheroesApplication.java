91f59cd95bd5514097d85d384e56d75014139759
Alexandru Raducanu <raducanu.alexandru2001@gmail.com>
2023-07-28 14:40:08 +0100

    Done QL5)

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,12 +1,14 @@
 package com.example.superheroes;
 
+import com.example.superheroes.model.Hero;
 import com.example.superheroes.practice.Calculator;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
@@ -16,25 +18,24 @@ public class SuperheroesApplication implements CommandLineRunner {
 	}
 
 	@Autowired
+	HeroService heroService;
+
+
+	@Autowired
 	Calculator calc;
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Bonjour, world!");
 
-		int result = calc.addition(3,4);
-		System.out.println(result);
 
+		List<Hero> heroes = heroService.getLastThreeHeroesByName();
+		for (Hero hero : heroes )
+		{
+			System.out.println(hero);
+		}
 
 	}
 
-	@PostConstruct
-	public void init() {
-		System.out.println("Started Bean");
-	}
 
-	@PreDestroy
-	public void destroy() {
-		System.out.println("Calcuator will be destroyed");
-	}
 
 }
--- superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
+++ superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
@@ -1,5 +1,7 @@
 package com.example.superheroes.practice;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,4 +11,14 @@ public class Calculator {
         return num1+num2;
 
     }
+    @PostConstruct
+    public void init() {
+        System.out.println("Started Bean");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Calcuator will be destroyed");
+    }
+
 }
