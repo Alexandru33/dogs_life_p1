671b594bef27d06c156b7835f75eea2a08b7c321
Alexandru Raducanu <raducanu.alexandru2001@gmail.com>
2023-07-28 11:31:14 +0100

    Done QL3)

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,5 +1,9 @@
 package com.example.superheroes;
 
+import com.example.superheroes.practice.Calculator;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +15,26 @@ public class SuperheroesApplication implements CommandLineRunner {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 
+	@Autowired
+	Calculator calc;
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Bonjour, world!");
+
+		int result = calc.addition(3,4);
+		System.out.println(result);
+
+
 	}
+
+	@PostConstruct
+	public void init() {
+		System.out.println("Started Bean");
+	}
+
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Calcuator will be destroyed");
+	}
+
 }
