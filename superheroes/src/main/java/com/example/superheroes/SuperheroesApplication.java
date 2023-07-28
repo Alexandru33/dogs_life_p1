ae829e5bd690b08f3d765e8f44fc7586f976fee7
unknown <chirca_virginia@yahoo.com>
2023-07-28 14:05:17 +0100

    QL7

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,7 +1,7 @@
 package com.example.superheroes;
-import com.example.superheroes.practice.Calculator;
 import com.example.superheroes.repository.HeroRepository;
-import model.Hero;
+import com.example.superheroes.model.Hero;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,19 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner {
-	@Autowired
-	HeroRepository heroRepository;
+public class SuperheroesApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
-	@Override
-	public void run(String... args) throws Exception {
-		List<Hero> heroes = heroRepository.findAll();
-		for (Hero hero: heroes) {
-			System.out.println(hero);
-		}
-}
+
 
 
 }
