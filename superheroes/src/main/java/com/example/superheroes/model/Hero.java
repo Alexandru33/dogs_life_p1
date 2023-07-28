590e7f00c86e470e0a9010e8ae495b752655b5d2
Alexandru Raducanu <raducanu.alexandru2001@gmail.com>
2023-07-28 11:39:28 +0100

    Middle of QL4)

--- superheroes/src/main/java/com/example/superheroes/model/Hero.java
+++ superheroes/src/main/java/com/example/superheroes/model/Hero.java
@@ -1,4 +1,23 @@
 package com.example.superheroes.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "heroes")
 public class Hero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String alias;
+
+    private String superpower;
+
+    private int teamid;
 }
