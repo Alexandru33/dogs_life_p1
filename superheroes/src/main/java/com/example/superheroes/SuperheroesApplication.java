45cc4b635f5245e828975bdfc5ea6085bbf2804e
neeharikam25 <neeharikamittra702@gmail.com>
2023-07-28 13:53:08 +0100

    ql3

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,14 +1,20 @@
 package com.example.superheroes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.superheroes.practice.Calculator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
+
+	@Autowired
+	Calculator calc;
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World");
+		System.out.println(calc.addition(1,2));
 	}
 
 	public static void main(String[] args) {
