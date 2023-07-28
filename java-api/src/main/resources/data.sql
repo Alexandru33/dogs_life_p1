59d1e43a0d4269a7e3fe78fada91e9f185308d04
Alexandru Raducanu <raducanu.alexandru2001@gmail.com>
2023-07-28 15:43:27 +0100

    Solved .sql files

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-INSERT INTO owners
-VALUES
-(1, 'Alex'),
-(2, 'Bob')
-;
-
-INSERT INTO dogs
-VALUES
-(1, 'Dog_John', 10 , 1),
-(2, 'Dog_Kyle', 7 , 2)
-;
-=======
 INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
 INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
 INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
->>>>>>> feature/dogs_tdd
