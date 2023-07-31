3ded3ce1b89b5fa29b4a91b6db1109dcc9d1689b
Xizhen Huang <sizenrhuang@outlook.com>
2023-07-31 13:24:57 +0100

    init

---
+++ superheroes-ui/src/components/HeroDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroDetail = () => {
+  return (
+    <div>
+        <img src="images/batman.jpeg" alt="Batman" height="250px"/>
+        <p id="blue-alias">Alias: Batman</p>
+        <p class="green-class">Name: Bruce Wayne</p>
+        <button class="green-class">Like</button> 
+    </div>
+  )
+}
+
+export default HeroDetail
