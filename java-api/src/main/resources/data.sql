2b9663ad7dde001eec30375a1b96f973c12087dd
Alexandru <raducanu.alexandru2001@gmail.com>
2023-08-01 15:40:41 +0100

    Sper ca merge acum

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,11 @@
 INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
 INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
 INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+
+INSERT INTO owners (owner_id, name) VALUES ( 1, 'Steve');
+INSERT INTO owners (owner_id, name) VALUES ( 2, 'Robert');
+INSERT INTO owners (owner_id, name) VALUES ( 3, 'Ana');
+
+INSERT INTO dog_n_owners (id , dog_owner , dog) VALUES ( 1, 'Steve', 'Einstein' );
+INSERT INTO dog_n_owners (id , dog_owner , dog) VALUES ( 2, 'Steve', 'Kaya' );
+INSERT INTO dog_n_owners (id , dog_owner , dog) VALUES ( 3, 'Robert', 'Lassie' );
